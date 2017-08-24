c9800959f413c5aa3742a9f4e49cb84cdf90c1c5
Jef <summerlzf123@126.com>
2017-08-25 02:53:17 +0800

    更新list转map

--- src/main/java/com/tuandai/tava/feature/tomap/ToMapDemo.java
+++ src/main/java/com/tuandai/tava/feature/tomap/ToMapDemo.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -26,10 +27,14 @@ public class ToMapDemo {
         return list.stream().collect(Collectors.toMap(UserInfo::getId, v -> v));
     }
 
+    public Map<String, UserInfo> new2(List<UserInfo> list) {
+        return list.stream().collect(Collectors.toMap(UserInfo::getId, Function.identity()));
+    }
+
     @Test
     public void test() {
         List<UserInfo> list = UserInfoFactory.create();
-        Map<String, UserInfo> map = new1(list);
+        Map<String, UserInfo> map = new2(list);
         map.forEach((key, val) -> {
             System.out.println(key + "  ->  UserInfo.id: " + val.getId() + "   username: " + val.getUsername());
         });
